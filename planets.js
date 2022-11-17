4f0e31bc7ec7dcf6d72ea401e8f0641c12bdecdb
camilleharmon <camille.harmon@mullenmail.com>
2022-11-17 14:05:23 -0700

    first commit

---
+++ planets.js
@@ -0,0 +1,10 @@
+
+    function terraform(){
+
+        document.getElementById("Background").setAttribute("style", "color: white")
+        document.getElementById("Star").innerHTML = "I'm a blackhole!"
+        document.getElementById("Star").setAttribute("style", "color: grey")
+        
+        
+    }
+
